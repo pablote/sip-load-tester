c118879940abfa241e70609f9d2ba8c95327ec08
Pablo Compagni <pablocompagni@gmail.com>
2016-09-23 14:57:34 -0300

    cleaned up config

--- config.js
+++ config.js
@@ -1,40 +1,11 @@
 module.exports = {
     totalCallsCount: 400,
     callsPerSecond: 10,
-    origins: [
-        {
-            server: '104.193.20.18',
+    origins: [{
+            server: '1.1.1.1',
             enabled: true,
-            username: 'freeswitch',
-            password: 'works',
-            weight: 30
-        },
-        {
-            server: '10.0.82.91',
-            enabled: true,
-            username: 'freeswitch',
-            password: 'works',
-            weight: 10
-        },
-        {
-            server: '10.0.82.92',
-            enabled: true,
-            username: 'freeswitch',
-            password: 'works',
-            weight: 10
-        },
-        {
-            server: '104.193.20.11',
-            enabled: true,
-            username: 'freeswitch',
-            password: 'works',
-            weight: 10
-        },
-        {
-            server: '104.193.20.12',
-            enabled: true,
-            username: 'freeswitch',
-            password: 'works',
+            username: 'username',
+            password: 'password',
             weight: 10
         }
     ],
