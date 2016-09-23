c4f78b1c2021a25200cfa98fa867bb01ed409e48
Pablo Compagni <pablocompagni@gmail.com>
2016-09-23 13:36:35 -0300

    using config file

--- app.js
+++ app.js
@@ -4,23 +4,20 @@ var url = require('url');
 var RateLimiter = require('limiter').RateLimiter;
 var rsvp = require('rsvp');
 var request = require('request');
+var config = require('./config');
 
-var n = 20;
-var cps = 2;
-var indexes = Array.apply(null, {length: n}).map(Number.call, Number);
-var limiter = new RateLimiter(cps, 'second');
+var indexes = Array.apply(null, {length: config.totalCallsCount}).map(Number.call, Number);
+var limiter = new RateLimiter(config.callsPerSecond, 'second');
 var requestPromises = [];
 
 indexes.forEach(function (index) {
     requestPromises.push(new rsvp.Promise(function (resolve, reject) {
         limiter.removeTokens(1, function (err, remainingRequests) {
-            var conferencePin = '9047';
-            var originServer = '104.193.20.18';
-            var destinationServer = '104.193.20.18';
-            var destinationNumber = '6463500992';
-            var baseUrl = util.format('http://freeswitch:works@%s:8080/', originServer);
+            var baseUrl = util.format('http://freeswitch:works@%s:8080/', config.origins[0].server);
             var urlPath = util.format('webapi/bgapi?lua summondialer.lua robot %s@%s 123 sleep:8000|dtmf:%s',
-                destinationNumber, destinationServer, conferencePin);
+                config.destination.number,
+                config.destination.server,
+                config.destination.conferencePin);
 
             request(url.resolve(baseUrl, encodeURIComponent(urlPath)), function (error, response, body) {
                 if (error) {
---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    totalCallsCount: 20,
+    callsPerSecond: 2,
+    origins: [
+        {
+            server: '104.193.20.18'
+        }
+    ],
+    destination: {
+        conferencePin: '9047',
+        server: '104.193.20.18',
+        number: '6463500992'
+    }
+};
